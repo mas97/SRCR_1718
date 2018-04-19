7c62fe3de7637b7eba4e0ca12ef02cee660a18c4
TheGuyInTheClouds <33250763+SilentLynx@users.noreply.github.com>
2018-04-19 15:03:34 +0100

    substitui todos os uncert* por incert*

--- Exercicio2/base_conhecimento.pl
+++ Exercicio2/base_conhecimento.pl
@@ -49,11 +49,11 @@ utente( 8, luis, 35, lisboa).
         nao( excecao( utente( IDU, N, I, M ) ) ).
 
 % Conhecimento incerto
-utente( 9, ana, 22, uncert1).
-excecao( utente(IDU, No, I, M)) :- utente(IDU, No, I, uncert1).
+utente( 9, ana, 22, incert1).
+excecao( utente(IDU, No, I, M)) :- utente(IDU, No, I, incert1).
 
-utente( 10, beatriz, 43, uncert2).
-excecao( utente(IDU, No, I, M)) :- utente(IDU, No, I, uncert2).
+utente( 10, beatriz, 43, incert2).
+excecao( utente(IDU, No, I, M)) :- utente(IDU, No, I, incert2).
 
 % Conhecimento impreciso
 excecao( utente(11, bernardo, 15, felgueiras)).
@@ -104,11 +104,11 @@ prestador( 7, paulo, tecnicoRaioX, 6).
 	nao( excecao( prestador( ID, No, E, I))).
 
 % Conhecimento incerto
-prestador( 8, pedro, uncert3, 5).
-excecao( prestador(IDP, No, E, IDI)) :- prestador(IDP, No, uncert3, IDI).
+prestador( 8, pedro, incert3, 5).
+excecao( prestador(IDP, No, E, IDI)) :- prestador(IDP, No, incert3, IDI).
 
-prestador( 9, carolina, uncert4, 3).
-excecao( prestador(IDP, No, E, IDI)) :- prestador(IDP, No, uncert4, IDI).
+prestador( 9, carolina, incert4, 3).
+excecao( prestador(IDP, No, E, IDI)) :- prestador(IDP, No, incert4, IDI).
 
 
 % Conhecimento impreciso
@@ -165,14 +165,14 @@ cuidado( 2018/10/22, 8, 1, consulta, 20).
 
 
 % Conhecimento incerto
-cuidado( 2018/01/20, 7, 4, uncert5, 25).
-excecao( cuidado(D, IDU, IDP, De, C)) :- cuidado(D, IDU, IDP, uncert5, C).
+cuidado( 2018/01/20, 7, 4, incert5, 25).
+excecao( cuidado(D, IDU, IDP, De, C)) :- cuidado(D, IDU, IDP, incert5, C).
 
-cuidado( 2018/04/10, 2, 3, consulta, uncert6).
-excecao( cuidado(D, IDU, IDP, De, C)) :- cuidado(D, IDU, IDP, De, uncert6).
+cuidado( 2018/04/10, 2, 3, consulta, incert6).
+excecao( cuidado(D, IDU, IDP, De, C)) :- cuidado(D, IDU, IDP, De, incert6).
 
-cuidado( uncert7, 5, 2, penso, 5).
-excecao( cuidado(D, IDU, IDP, De, C)) :- cuidado(uncert7, IDU, IDP, De, C).
+cuidado( incert7, 5, 2, penso, 5).
+excecao( cuidado(D, IDU, IDP, De, C)) :- cuidado(incert7, IDU, IDP, De, C).
 
 
 % Conhecimento impreciso
@@ -210,7 +210,6 @@ instituicao( 3, hospitalsantamaria, porto).
 instituicao( 4, hospitaltrofa, porto).
 instituicao( 5, centrosaudegualtar, braga).
 instituicao( 6, hospitalaveiro, aveiro).
-instituicao( 7, uncert8, guimaraes).
 
 % Conhecimento negativo
 -instituicao(1, hospitalbraga, guimaraes).
@@ -227,7 +226,8 @@ instituicao( 7, uncert8, guimaraes).
 
 
 % Conhecimento incerto
-excecao( instituicao(IDI, No, Ci)) :- instituicao(IDI, uncert8, Ci).
+instituicao( 7, incert8, guimaraes).
+excecao( instituicao(IDI, No, Ci)) :- instituicao(IDI, incert8, Ci).
 
 % Conhecimento impreciso
 excecao( instituicao(8, centrosaudeguimaraes, Ci)) :- Ci == amares.
